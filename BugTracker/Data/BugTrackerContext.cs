bbf3a979ee919be901f62de02d1085f0acadee05
gecfan <tsod2000@gmail.com>
2022-03-24 08:28:53 -0400

    Add project files.

---
+++ BugTracker/Data/BugTrackerContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BugTracker.Models;
+
+namespace BugTracker.Data
+{
+    public class BugTrackerContext : DbContext
+    {
+        public BugTrackerContext (DbContextOptions<BugTrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BugTracker.Models.Bug> Bug { get; set; }
+    }
+}
---
+++ BugTracker/Models/Bug.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateSubmitted { get; set; }
+        public string? Category {  get; set; }
+        public string? Description { get; set; }
+    }
+}
